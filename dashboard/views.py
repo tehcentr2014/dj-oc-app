7b9b8592569d56354deac95c1f67d9f9ebc8d732
Mary na <tehcentr2014@gmail.com>
2024-06-06 12:46:16 +0000

    Profile update

--- dashboard/views.py
+++ dashboard/views.py
@@ -20,7 +20,19 @@ def home (request):
     return render(request, 'dashboard/home.html', context)
 
 def profile(request):
+
     context = {}
-    #form = ProfileForm()
+
+    if request.method == 'GET':
+        form = ProfileForm()
+        context['form'] = form
+        return render(request, 'dashboard/profile.html', context)
+
+    if request.method == 'POST':
+        form  =  ProfileForm(request.POST)
+
+        if form.is_valid():
+            pass
+
 
     return render(request, 'dashboard/profile.html', context)
